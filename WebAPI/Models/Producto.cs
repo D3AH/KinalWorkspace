08fc89659004211baea828094b7bde99cc97c665
D3AH <diegoeauyonh@gmail.com>
2018-09-10 10:59:27 -0600

    Adding Entity Framework & Class Producto

---
+++ WebAPI/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Producto //Model data
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+    }
+    // This class generate the database, this feature is called "FirstCode" a part of EntityFramework
+
+    class ProductoDbContest : DbContext
+    {
+
+    }
+}
